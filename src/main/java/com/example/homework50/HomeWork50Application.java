dd30c340dfabc756a428dca7a2b43d212512d881
Kudabaev Dzhusupbek <jokakudabaev@gmail.com>
2022-09-17 17:08:44 +0600

    initial commit

---
+++ src/main/java/com/example/homework50/HomeWork50Application.java
@@ -0,0 +1,13 @@
+package com.example.homework50;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork50Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork50Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework50/HomeWork50ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework50;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork50ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
